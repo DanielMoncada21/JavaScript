a8942ab8b9f632034b5229c4d5364df128a600ee
DanielMoncada21 <moncadadaniel198@gmail.com>
2025-03-29 09:31:51 -0500

    subiendo

---
+++ Ejercicio.js
@@ -0,0 +1,31 @@
+console.log (eval(new String("2 + 2"))); 
+console.log ('\n');
+console.log (eval("2 + 2"));
+
+var nombre="Hola mundo";
+
+console.log(nombre);
+
+
+var variable="nombre";
+
+let nombre2;
+nombre2="Daniel";
+const apellido="Aguilar";
+console.log(nombre2);
+console.log(apellido);
+let nombrecompleto2="Sebastian vanegas";
+let nombreCompleto2="Daniel Aguilar";
+console.log(nombrecompleto2);
+console.log(nombreCompleto2);
+ let _232;
+ let $fjshdjf;
+ let ashja;
+ console.log(_232);
+ console.log($fjshdjf);
+ console.log(ashja);
+ let x, y;
+ x= 5;
+ y=3;
+ console.log(x,y);
+ 
