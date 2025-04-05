4692953c542e04cb3531f57af534f91e437d0a36
DanielMoncada21 <moncadadaniel198@gmail.com>
2025-04-04 19:48:44 -0500

    subiendo

---
+++ operaciones.js
@@ -0,0 +1,66 @@
+let a=1000000;
+let b=1000000;
+let z=a+b;
+console.log(z)
+z=a-b;
+console.log(z)
+z=a*b;
+console.log(z)
+z= a/b;
+console.log(z)
+z=a%b;
+console.log(z)
+z=a**b
+console.log(z)
+
+let i=0;
+console.log(++i)
+let j=0;
+console.log(j++)
+console.log(j)
+console.log(j++)
+console.log(j++)
+let k=3
+console.log(--k)
+let l=3 
+console.log(l--)
+console.log(l--)
+
+let r =1
+r += 3
+console.log(r)
+r -= 2
+console.log(r)
+r *= 5
+console.log(r)
+r /= 3
+console.log(r)
+
+a=3
+b=2
+c="3"
+d=3
+console.log(a==c)
+console.log(a===c)
+console.log(a!=c)
+console.log(a!==c)
+
+let resultado=(1 > 2) ? "verdadero": "falso";
+console.log(resultado)
+let resultado2=(1>2) ? "verdadero":0;
+console.log(resultado2)
+let numero=10
+resultado=(numero%2==0) ?"par":"impar";
+console.log(resultado)
+
+let a1=11
+let minimo=0
+let maximo=10
+if(a >= minimo && a <= maximo){
+    console.log("El numero esta en el rango")
+}
+else {
+    console.log("El numero esta fuera del rango")
+}
+
+ 
